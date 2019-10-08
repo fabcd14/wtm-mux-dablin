3d2e0e4814828e15e406688438e026aacb795d16
fabcd14 <50106444+fabcd14@users.noreply.github.com>
2019-10-08 10:58:35 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,11 +14,15 @@ sudo apt-get -y install git gcc g++ cmake
 sudo apt-get -y install libmpg123-dev libfaad-dev libsdl2-dev libgtkmm-3.0-dev
 sudo apt-get -y install libfdk-aac-dev libzmq3-dev libzmq5
 
+echo -e "$GREEN Creation of the dab directory $NORMAL"
+mkdir ~/dab
+chmod +c launch.sh
+cp launch.sh ~/dab
+
 echo -e "$GREEN Installing DabLin $NORMAL"
 sudo apt-get -y install dablin 
 
 echo -e "$GREEN Installing ZMQ connector $NORMAL"
-mkdir ~/dab
 cd ~/dab
 git clone https://github.com/mpbraendli/mmbtools-aux
 cd mmbtools-aux/zmqtest/zmq-sub
@@ -27,4 +31,6 @@ sudo cp zmq-sub /usr/bin
 
 echo -e "$GREEN Done installing all tools $NORMAL"
 echo -e "ZMQ connector has been dowloaded to the dab/ folder"
-
+echo -e "In order to launch DABLin with WTM trials,"
+echo -e "just execute the launch.sh script located"
+echo -e "in the $HOME/dab directory"
